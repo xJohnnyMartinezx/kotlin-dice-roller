91bec560d96cb839a8fa8fd11e4b6aa225006a2b
Johnny <mrtnz.jn@gmail.com>
2023-05-07 16:35:00 -0500

    crated first dice inatance and called makeRoll methods which contains random num gen

--- src/main/kotlin/DiceRoller.kt
+++ src/main/kotlin/DiceRoller.kt
@@ -1,14 +1,36 @@
 package main.kotlin
 
 fun main(){
+
+//    ********** RANDOM NUM GENERATOR **********
 //    println("test");
+//
+//    val diceRange = 1..6;
+//
+//    val randomNum = diceRange.random();
+//
+//    println("Random number is: ${randomNum}");
+//
+//
 
-    val diceRange = 1..6;
+//    **** INITIALIZING DICE CLASS
+    val myFirstDice = Dice();
 
-    val randomNum = diceRange.random();
+    println(myFirstDice.sides);
+    println(myFirstDice.makeRoll());
 
-    println("Random number is: ${randomNum}");
+}
 
+class Dice{
+
+//    PROPERTIES
+    val sides = 6;
+
+//    METHODS
+    fun makeRoll(){
+        val randomNum = (1..6).random();
+        println(randomNum);
+    }
 
 
 
