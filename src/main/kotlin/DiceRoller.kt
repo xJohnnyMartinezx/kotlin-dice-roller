39f5b31e2b88e74ad7f1fc7d3dac482f378e289a
Johnny <mrtnz.jn@gmail.com>
2023-05-08 14:02:01 -0500

    created conditionals to check if random number matches lucky number

--- src/main/kotlin/DiceRoller.kt
+++ src/main/kotlin/DiceRoller.kt
@@ -18,11 +18,43 @@ fun main(){
     val mySecondDice = Dice(20)
 
     val diceRoll = myFirstDice.makeRoll();
-    val diceRollTwo = mySecondDice.makeRoll();
+//    val diceRollTwo = mySecondDice.makeRoll();
 
-    println("Your ${myFirstDice.numSides} sided dice rolled a $diceRoll");
+//    println("Your ${myFirstDice.numSides} sided dice rolled a $diceRoll");
+//
+//    println("Your ${mySecondDice.numSides} sided dice rolled a $diceRollTwo");
 
-    println("Your ${mySecondDice.numSides} sided dice rolled a $diceRollTwo");
+    val luckyNum = 4;
+    println("$diceRoll : 6 lucky num is: $luckyNum");
+
+    when (diceRoll){
+        luckyNum -> println("You win!");
+        1 -> println("you rolled a 1, try again.");
+        2 -> println("you rolled a 2, try again.");
+        3 -> println("you rolled a 3, try again.");
+        5 -> println("you rolled a 5, try again.");
+        6 -> println("you rolled a 6, try again.");
+
+
+    }
+
+
+
+//    println("$diceRollTwo : 20 lucky num is: $luckyNum");
+
+//    if (diceRoll == luckyNum){
+//        println("You're in luck. the numbers match!");
+//    }else if (diceRoll == 1){
+//        println("you rolled a 1, try again.");
+//    }else if (diceRoll == 2){
+//        println("you rolled a 2, try again.");
+//    }else if (diceRoll == 3){
+//        println("you rolled a 3, try again.");
+//    }else if (diceRoll == 5){
+//        println("you rolled a 5, try again.");
+//    }else {
+//        println("you rolled a 6, try again.");
+//    }
 
 
 
